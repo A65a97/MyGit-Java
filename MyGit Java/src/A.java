fe1d67f6f90b708477a2479315a795db352b8a19
BitJoon <BitJoon@DESKTOP-BitJoon>
2020-06-12 20:27:53 +0900

    devB = change A.java B.java

--- MyGit Java/src/A.java
+++ MyGit Java/src/A.java
@@ -2,6 +2,7 @@
 public class A {
 	public static void main(String args[]) {
 		System.out.println("Hello, My name is Ajava");
+		System.out.println("Let me introduce my friends!\n");
 		new B().callB();
 		new C().callC();
 	}
--- MyGit Java/src/B.java
+++ MyGit Java/src/B.java
@@ -2,5 +2,6 @@
 public class B {
 	public void callB() {
 		System.out.println("Hello, I'm Bee~");
+		System.out.println("Nice to meet you~\n");
 	}
 }

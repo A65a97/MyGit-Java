c35f231a4fd65f3aec5d5796feed810eacfd9c4f
BitJoon <BitJoon@DESKTOP-BitJoon>
2020-06-12 20:25:28 +0900

    change A.java

--- MyGit Java/src/A.java
+++ MyGit Java/src/A.java
@@ -2,7 +2,9 @@
 public class A {
 	public static void main(String args[]) {
 		System.out.println("Hello, My name is Ajava");
-		new B().callB();
-		new C().callC();
+		B myB = new B();
+		C myC = new C();
+		myB.callB();
+		myC.callC();
 	}
 }
